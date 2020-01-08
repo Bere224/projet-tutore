95a9c120856884ba148428013a61251bdda2878b
Bere224 <59656425+Bere224@users.noreply.github.com>
2020-01-08 19:06:34 +0100

    Update newcategorie.php

--- newcategorie.php
+++ newcategorie.php
@@ -10,15 +10,10 @@ require_once("categorieFactory.php");
 $nom = $_POST["nom"];
 $description = $_POST["description"]
 
-
-
-/*$result = mysqli_query($co, 'INSERT INTO groupe(libelle,dateCreation,ID) VALUES(\'$nomgrp\',\'$datecreation\', 1)') or die ("Exécution de la requête impossible".mysqli_error($co));
-echo 'test2';
-header('Location: creergroupe.php');*/
-
-
 $cat = categorieFactory::creercategorie($nom,$description);
 
 $cat->ajouter_dans_db();
+
+header('Location : creercategorie.php');
 
 ?>
