89793beec7c91c2c4d634bdaf3c94afd7c30c760
Lucy Kueny <lucy.kueny@posteo.net>
2020-01-08 19:14:11 +0100

    Correction date, mysql n'aime pas le vieux

--- newpropo.php
+++ newpropo.php
@@ -17,7 +17,7 @@ $idGroupe = $_GET['idGroupe'];
 
 if($_POST['datechoix'] = 'sans')
 {
-	$datedecloture = '0000-00-00';
+	$datedecloture = '1999-00-00';
 }
 
 $propo = propositionFactory::creer($nompropo,$desccourte,$desclongue,$catprinc,$catsec,$datedecloture,$idConnecte,$idGroupe);
