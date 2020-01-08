5ba69b7a9a9588a41bb2046ea868d14dd5545358
tplulu <59656493+tplulu@users.noreply.github.com>
2020-01-08 18:45:56 +0100

    b
    nj

--- voirgroupe.php
+++ voirgroupe.php
@@ -3,19 +3,19 @@ require_once("connect.php");
 
 require_once("utilisateur.php");
 require_once("utilisateurFactory.php");
-
+require_once("listeGroupeFactory.php");
+require_once("listeGroupe.php");
+require_once("GroupeFactory.php");
 if($idConnecte==-1){
     header("Location:home.php");
 }
 ?>
 
-
-
 <!doctype html>
 <html lang="fr">
     <head>
         <meta charset="utf-8">
-        <title>accueil </title>
+        <title>voirgroupe.php </title>
         <link rel="stylesheet" type="text/css" href="css2.css" />
     </head>
     <body>
@@ -23,38 +23,43 @@ if($idConnecte==-1){
         require_once("listeGroupeFactory.php");
         require_once("listeGroupe.php");
 
-        require_once("email.php");
+        require_once("listePropositonFactory.php");
+        require_once("propositionFactory.php");
+		 require_once("proposition.php");
+        require_once("listeCommentaireFactory.php");
+		 require_once("listeCommentaire.php");
         require_once("emailFactory.php");
-
-        
-        ?>
+		?>
 		
-		<?php $utilisateur= utilisateurFactory::charger($idConnecte); 
-$Nom= $utilisateur->getNom(); 
-$prenom= $utilisateur->getPrenom(); 
+	<?php 	
+		$id=$_POST['iddugroupe'];
+		$grp=groupeFactory::charger($id); 
+		$Nomgrp= $grp->getNom();
+	?>
 
+<h1>  <?php echo "$Nomgrp" ?> </h1>;
 
-?>
-<h1>  Bienvenue <?php echo "$Nom $prenom" ?> </h1>;
+	
 
-		<h2> vos groupes : </h2>
-        <form method="post" action="voirgroupe.php" > 
+		<h2> listes des propositions : </h2>
+        <form method="post" action="voirpropo.php" > 
             <?php
-            $listeGroupe = listeGroupeFactory::listeGroupesPourUtilisateur($idConnecte);
+            $listepropo = listePropositionFactory::listePropositionPourGroupe($id);
             ?>
             <table>
                 <tr>
                     <th>id</th>
                     <th>libelle</th>
+					<th>description courte</th
                 </tr>
                 <?php  //On affiche les lignes du tableau une à une à l'aide d'une boucle
-                foreach ($listeGroupe->getGroupes() as $groupe)
+                foreach ($listepropo->getProposition() as $proposition)
                 {
                 ?>
                 <tr>
-                    <td><?php echo $groupe->getId() ?></td>
-                    <td><button type="SUBMIT" name="iddugroupe" value="<?php echo $groupe->getId() ?>"> <?php echo $groupe->getNom() ?></button> </td>
-                   
+                    <td><?php echo $proposition->getId() ?></td>
+                    <td><button type="SUBMIT" name="iddugroupe" value="<?php echo $proposition->getId() ?>"> <?php echo $proposition->getNom() ?></button> </td>
+                    <td> <?php echo $proposition->getDescriptionCourte() ?> </td>
                 </tr>
                 <?php } ?>
             </table>
@@ -65,7 +70,7 @@ $prenom= $utilisateur->getPrenom();
 		<h2> groupes possédés: </h2>
         <form method="post" action="groupeadmin.php" > 
             <?php
-            $listeGroupe = listeGroupeFactory::listeGroupesPossedeParUtilisateur($idConnecte);
+            $listecommentaires = listeGroupeFactory::listeGroupesPossedeParUtilisateur($idConnecte);
             ?>
             <table>
                 <tr>
@@ -84,20 +89,6 @@ $prenom= $utilisateur->getPrenom();
                 <?php } ?>
             </table>
         </form>
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 		
 		
 
